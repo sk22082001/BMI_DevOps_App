bc0468953ee74c295767baa40a728d544edcedf7
sk22082001 <165132458+sk22082001@users.noreply.github.com>
2024-03-27 15:13:18 +0530

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'node', // Specify the test environment if needed
+  globals: {
+    'ts-jest': {
+      ecmaVersion: 2019, // Specify the ECMAScript version here
+    },
+  },
+};
